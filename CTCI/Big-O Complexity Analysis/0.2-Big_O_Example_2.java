0e342fcb79222a673605f3dff63770c38f634166
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-12 13:22:07 -0600

    complete Q0.2

---
+++ CTCI/Big-O Complexity Analysis/0.2-Big_O_Example_2.java
@@ -0,0 +1,25 @@
+/* Big-O Question 0.2 
+ * Example 2 */
+
+/* Question: What is the runtime of the below code? */
+class Question {
+    void printPairs(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                System.out.println(array[i] + "," + array[j]);
+            }
+        }
+    }
+}
+
+/* Solution */
+
+/*
+ * Time Complexity:
+ * 
+ * outer loop: O(n)
+ * inner loop: O(n)
+ * 
+ * time complexity: O(n) * O(n) = O(n^2)
+ * 
+ */
