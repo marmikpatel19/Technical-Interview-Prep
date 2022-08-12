62a878bc3d6d13d62f6215531bb546e4ceeb7619
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-12 13:13:41 -0600

    complete Q0.1

---
+++ CTCI/Big-O Complexity Analysis/0.1-Big_O_Example_1.java
@@ -0,0 +1,30 @@
+/* Big-O Question 0.1 
+ * Example 1 */
+
+/* Question: What is the runtime of the below code? */
+class Question {
+    void foo(int[] array) {
+        int sum = 0;
+        int product = 1;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        for (int i = 0; i < array.length; i++) {
+            product *= array[i];
+
+            System.out.println(sum + ", " + product);
+        }
+    }
+}
+
+/* Solution */
+
+/*
+ * Time Complexity:
+ * 
+ * for loop 1: O(n)
+ * for loop 2: O(n)
+ * 
+ * time complexity: O(n) + O(n) = O(n)
+ * 
+ */
