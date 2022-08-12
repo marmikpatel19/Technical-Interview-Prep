d340b5ffde66337e212b6435f980f3e11876d78a
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-12 14:00:28 -0600

    complete Q0.6

---
+++ CTCI/Big-O Complexity Analysis/0.6-Big_O_Example_6.java
@@ -0,0 +1,27 @@
+/* Big-O Question 0.6 
+ * Example 6 */
+
+/* Question: What is the runtime of the below code? */
+class Question {
+    void reverse(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int other = array.length - i - 1;
+            int temp = array[i];
+            array[i] = array[other];
+            array[other] = temp;
+        }
+    }
+}
+
+/* Solution */
+
+/*
+ * Time Complexity:
+ * 
+ * outer loop: O(n)
+ * number of iteration is n/2 => O(n)
+ * 
+ * 
+ * time complexity: O(n)
+ * 
+ */
