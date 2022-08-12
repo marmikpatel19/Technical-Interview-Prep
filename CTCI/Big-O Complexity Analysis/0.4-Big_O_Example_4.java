63ac6f09b1412d3de891a98c6b04c0b3f38549ed
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-12 13:53:13 -0600

    complete Q0.4

---
+++ CTCI/Big-O Complexity Analysis/0.4-Big_O_Example_4.java
@@ -0,0 +1,28 @@
+/* Big-O Question 0.4 
+ * Example 4 */
+
+/* Question: What is the runtime of the below code? */
+class Question {
+    void printUnorderedPairs(int[] arrayA, int[] arrayB) {
+        for (int i = 0; i < arrayA.length; i++) {
+            for (int j = 0; j < arrayB.length; j++) {
+                if (arrayA[i] < arrayB[j]) {
+                    System.out.println(arrayA[i] + arrayB[i]);
+                }
+            }
+        }
+    }
+}
+
+/* Solution */
+
+/*
+ * Time Complexity:
+ * 
+ * outer loop: O(n), where n is length of arrayA
+ * inner loop: O(m), where m is the length of arrayB
+ * 
+ * 
+ * time complexity: O(nm)
+ * 
+ */
