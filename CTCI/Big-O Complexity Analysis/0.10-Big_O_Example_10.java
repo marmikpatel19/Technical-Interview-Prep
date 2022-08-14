5ccf3919004873272a268940977d6d4ff0a05524
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-14 11:42:32 -0600

    complete Q0.10

---
+++ CTCI/Big-O Complexity Analysis/0.10-Big_O_Example_10.java
@@ -0,0 +1,32 @@
+/* Big-O Question 0.10 
+ * Example 10 */
+
+/* The following method checks if a number is prime by checking for divisibility on numbers less than it. It only
+needs to go up to the square root of n because if n is divisible by a number greater than its square root then
+it's divisible by something smaller than it.
+For example, while 33 is divisible by 11 (which is greater than the square root of 33), the "counterpart" to 11
+is 3 (3 * 11 = 33). 33 will have already been eliminated as a prime number by 3.
+
+Question: What is the time complexity of this function? */
+class Question {
+    boolean isPrime(int n) {
+        for (int x = 2; x * x <= n; x++) {
+            if (n % x == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+/* Solution */
+
+/*
+ * Time Complexity:
+ * 
+ * loop: x * x <= n <=> x < sqrt(n) => x goes from 2 to sqrt(n) by incrementing
+ * by one => O(sqrt(n))
+ * 
+ * time complexity: O(sqrt(n))
+ * 
+ */
