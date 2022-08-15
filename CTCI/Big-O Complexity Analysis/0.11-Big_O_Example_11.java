355bb2cf01393285c8f246c438479c8de114ba03
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-15 07:16:06 -0600

    complete Q0.11

---
+++ CTCI/Big-O Complexity Analysis/0.11-Big_O_Example_11.java
@@ -0,0 +1,26 @@
+/* Big-O Question 0.11
+ * Example 11 */
+
+/* Question: What is its time complexity? */
+class Question {
+    int factorial(int n) {
+        if (n < 0) {
+            return -1;
+        } else if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
+
+/* Solution */
+
+/*
+ * Time Complexity:
+ * 
+ * each recursive call is O(1) and there are n calls
+ * 
+ * time complexity: O(n)
+ * 
+ */
