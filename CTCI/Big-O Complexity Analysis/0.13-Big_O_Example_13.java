69bd81d1ed81a8a9c1d96fdae063e32d449b3bf2
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-15 12:54:12 -0600

    complete Q0.13

---
+++ CTCI/Big-O Complexity Analysis/0.13-Big_O_Example_13.java
@@ -0,0 +1,25 @@
+/* Big-O Question 0.13
+ * Example 13 */
+
+/* The following code computes the Nth Fibonacci number.
+Question: What is its time complexity? */
+class Question {
+    int fib(int n) {
+        if (n <= 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+/* Solution */
+
+/*
+ * Time Complexity:
+ * 
+ * Recursion with two branches: O(2^n)
+ * 
+ * time complexity:
+ * 
+ */
