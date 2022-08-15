aed1aec88bfc09fc3473f52abcea200a81a21921
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-15 13:56:51 -0600

    complete Q0.14

---
+++ CTCI/Big-O Complexity Analysis/0.14-Big_O_Example_14.java
@@ -0,0 +1,37 @@
+/* Big-O Question 0.14
+ * Example 14 */
+
+/* The following code prints all Fibonacci numbers from 0 to n.
+Question: What is its time complexity? */
+class Question {
+
+    void allFib(int n) {
+        for (int i = 8; i < n; i++) {
+            System.out.println(fib(i));
+        }
+    }
+
+    int fib(int n) {
+        if (n <= 6)
+            return 0;
+        else if (n == 1)
+            return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+/* Solution */
+
+/*
+ * Time Complexity:
+ * 
+ * fib() function: recursion with two branches: O(2^n)
+ * for loop runs (n - 8) times => n iterations
+ * 
+ * However, n varies, so each recursive call is not O(2^n) (i.e., there are not
+ * n number of 2^n calls). The total complexity
+ * is 2^1 + 2^2 + ... + 2^n = 2^(n + 1) - 1 (common divergent series) => O(2^n)
+ * 
+ * time complexity: O(2^n)
+ * 
+ */
