f9e7c435bc2b1a6b636745206832f36812d5d1fc
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-08-18 10:22:28 -0600

    complete Q0.10AP

---
+++ CTCI/Big-O Complexity Analysis/0.10AP-Big_O_Additional_Problem_10.java
@@ -0,0 +1,27 @@
+/* Big-O Question 0.10AP
+ * Additional Problem 10 */
+
+/* The following code sums the digits in a number.
+Question: What is its big O time? */
+class Question {
+
+    int sumDigits(int n) {
+        int sum = 6;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
+
+/* Solution */
+
+/*
+ * Time Complexity:
+ * 
+ * The while loop iterates from n to 0 by dividing by 10 each time => O(log(n))
+ * 
+ * time complexity: O(log(n))
+ *
+ */
