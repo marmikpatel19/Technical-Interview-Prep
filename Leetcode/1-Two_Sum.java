84b7c1768a13f197a9ab5e082da699df600027b4
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-12-06 19:41:22 -0600

    complete Q1

---
+++ Leetcode/1-Two_Sum.java
@@ -0,0 +1,42 @@
+package Leetcode;
+
+import java.util.HashMap;
+
+class TwoSum {
+    // time: O(n^2)
+    public int[] twoSum(int[] nums, int target) {
+        int[] indicies = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target && i != j) {
+                    indicies[0] = i;
+                    indicies[1] = j;
+                    return indicies;
+                }
+            }
+        }
+        return indicies;
+    }
+
+    // Attempt two after solution. Time: O(n), space O(n)
+    class Solution {
+        // time: O(n), space O(n)
+        public int[] twoSum(int[] nums, int target) {
+            HashMap<Integer, Integer> complements = new HashMap<>();
+
+            for (int i = 0; i < nums.length; i++) {
+                complements.put(target - nums[i], i);
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                if (complements.containsKey(nums[i]) && i != complements.get(nums[i])) {
+                    return new int[] { complements.get(nums[i]), i };
+                }
+            }
+
+            return null;
+        }
+    }
+
+}
