c47b1a74ff016b8eddea916153f3b92a5ae2aba2
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2022-12-07 16:44:57 -0600

    attempt 3 with dynamically-sized sliding window

--- Leetcode/3-Longest_Substring_Without_Repeating_Characters.java
+++ Leetcode/3-Longest_Substring_Without_Repeating_Characters.java
@@ -30,4 +30,40 @@ class LongestSubstringWithoutRepeatingCharacters {
 
         return largest;
     }
+
+    // Attempt two after solution
+
+    // dynamically-sized sliding window
+    // time: O(n), space: O(n)
+    // public int lengthOfLongestSubstring(String s) {
+    // if(s.length() == 0){
+    // return 0;
+    // }
+
+    // HashMap<Character, Integer> characters = new HashMap<>();
+
+    // int l = 0;
+    // int r = 0;
+    // int largest = 1;
+
+    // while(r < s.length()){
+
+    // // input current value at r into characters
+    // Character rightValue = s.charAt(r);
+    // characters.put(rightValue, characters.getOrDefault(rightValue, 0) + 1);
+
+    // // increment l until there are no repeated characters
+    // while(characters.get(rightValue) > 1){
+    // Character leftValue = s.charAt(l);
+    // characters.put(leftValue, characters.get(leftValue) - 1);
+    // l++;
+    // }
+
+    // largest = Math.max(largest, r - l + 1);
+
+    // r++;
+    // }
+
+    // return largest;
+    // }
 }
