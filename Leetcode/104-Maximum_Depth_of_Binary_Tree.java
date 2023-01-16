8cd7a6dedfb32260fde8c2cd203b83bcc061d177
Marmik Patel <90867690+marmikpatel19@users.noreply.github.com>
2023-01-16 12:49:57 -0600

    complete Q104

---
+++ Leetcode/104-Maximum_Depth_of_Binary_Tree.java
@@ -0,0 +1,30 @@
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class MaximumDepthofBinaryTree {
+    // time: O(n) where n is the number of nodes, space: O(1)
+    public int maxDepth(TreeNode root) {
+        // base case
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
