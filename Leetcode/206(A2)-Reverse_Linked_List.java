8c816c4446ce18a8844c612dbc88209095713d2d
Marmik Patel <marmikp19@gmail.com>
2023-12-18 09:44:11 +0530

    reattempt Q206

---
+++ Leetcode/206(A2)-Reverse_Linked_List.java
@@ -0,0 +1,34 @@
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        // pointers
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+}
